bc19fedfd59d5fe29d9c7b77c9d024009a4355dc
adi <aditya@mail.com>
2025-06-03 17:40:58 +0530

    Created custom hook for debouncing

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import productsApi from "apis/products";
 import Header from "components/commons/Header";
+import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData, Spinner } from "neetoui";
 import { isEmpty } from "ramda";
@@ -12,9 +13,10 @@ const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchKey, setSearchKey] = useState("");
+  const debouncedSearchKey = useDebounce(searchKey);
   const fetchProducts = async () => {
     try {
-      const data = await productsApi.fetch({ searchTerm: searchKey });
+      const data = await productsApi.fetch({ searchTerm: debouncedSearchKey });
       setProducts(data.products);
     } catch (error) {
       console.log("An error occurred:", error);
@@ -26,7 +28,7 @@ const ProductList = () => {
   useEffect(() => {
     fetchProducts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchKey]);
+  }, [debouncedSearchKey]);
 
   if (isLoading) {
     return (
