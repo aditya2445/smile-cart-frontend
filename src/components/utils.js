0745e70138c751f6a22b2f260ef5acad097844be
adi <aditya@mail.com>
2025-06-06 10:48:09 +0530

    Utilized babel-preset-neeto to access Zustand store properties

--- src/components/utils.js
+++ src/components/utils.js
@@ -3,6 +3,7 @@ import useCartItemsStore from "stores/useCartItemsStore";
 
 export const cartTotalOf = (products, priceKey) => {
   const { cartItems } = useCartItemsStore.getState();
+  console.log(cartItems);
 
   return sum(
     products.map(product => product[priceKey] * cartItems[product.slug])
